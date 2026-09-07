a2e8a76f2e6cc3bf434ecff1174092ce01cda2bb
SaithGulfam005 <gulfamsaith267@gmail.com>
2026-09-07 12:10:57 +0500

    footer set

--- components/config.php
+++ components/config.php
@@ -1,6 +1,6 @@
 <?php
 define('SITE_NAME', 'Digital Tutor Directory');
-define('BASE_URL', '');
+define('BASE_URL', '/digital-tutor-directory');
 
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/auth.php';
